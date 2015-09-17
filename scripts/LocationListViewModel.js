be955960428d2a0b87a6ef62c08ee4b820a69a43
Ben Plantinga <ben.plantinga@gmail.com>
2015-09-17 14:54:25 -0700

    Click on location list triggers info window and centering

--- scripts/LocationListViewModel.js
+++ scripts/LocationListViewModel.js
@@ -24,8 +24,13 @@
             var infowindow = new google.maps.InfoWindow({ content: self.info});
 
             google.maps.event.addListener(self.marker, 'click', function() {
+                self.marker.map.panTo(self.marker.position);
                 infowindow.open(googleMap, self.marker);
             });
+
+            self.clicked = function() {
+                google.maps.event.trigger(self.marker, 'click');
+            }
         };
     };
 
@@ -171,6 +176,7 @@
 
         self.searchResultsClicked = function(location) {
             console.log(location);
+            location.clicked();
         };
 
         // Center and resize map when window resized
